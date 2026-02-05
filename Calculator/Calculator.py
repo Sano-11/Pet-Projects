4cb9676d2a440ed4ba425e3e1111a16b0e60c3d5
Allen Tolete <allentolete@gmail.com>
2026-02-05 23:36:39 +0800

    Create Calculator.py

---
+++ Calculator/Calculator.py
@@ -0,0 +1,28 @@
+import math
+import tkinter
+
+
+class calculator:
+
+    def __init__(self):
+        pass
+
+    def create_widgets():
+        pass
+
+    def clear_Screen():
+        pass
+
+    def on_button_click():
+    ## this will serve as the input function
+    ## add a decimal guard, check if "." already exists in a string before another one
+        pass
+
+    def handle_operator():
+        pass
+
+    def perform_calculation():
+    ## check for math errors dito, like if denominator is 0
+        pass
+
+
