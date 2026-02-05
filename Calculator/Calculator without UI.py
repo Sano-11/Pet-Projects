09b3e9b14dbe86c41d417b7f4a9a451f7c2306db
Allen Tolete <allentolete@gmail.com>
2026-02-06 00:42:07 +0800

    Fixed Logic and Finalized

--- Calculator/Calculator without UI.py
+++ Calculator/Calculator without UI.py
@@ -1,46 +1,57 @@
-import math
-
-def add(x,y):
+def add(x, y):
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     return x - y
 
-def multiply(x,y):
+def multiply(x, y):
     return x * y
 
-def divide(x,y):
+def divide(x, y):
     return x / y
 
-print("""
-
-Calculator type shi
-
+while True:
+    print("""
 Select Operation
-
 [1] Add
 [2] Subtract
 [3] Multiply
 [4] Divide
-
 """)
-
-while True:
-
+    
     userchoice = input("Enter Choice: ")
-    if userchoice in ("1","2","3","4"):
+    
+    if userchoice in ("1", "2", "3", "4"):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
+
         except ValueError:
-            print ("Invalid Input, please enter a number")
+            ## IF num1 or num2 is a str, it displays this
+            print("Invalid Input, please enter a number")
             continue
-            
 
+        if userchoice == "1":
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+        elif userchoice == "2":
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+        elif userchoice == "3":
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+        elif userchoice == "4":
+            if num2 == 0:
+                print("Math Error, Cannot divide by zero.")
+                ## Why num2 only? 0/5 = 0 but 5/0 = math error
+                continue
+            else:
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
 
-
-
-
-
-
-
+        
+        continue_calculating = input(f"\nContinue Calculating? (Y/N): ").upper()
+        if continue_calculating == "N":
+            print("Exiting Calculator. Goodbye!")
+            break
+        elif continue_calculating == "Y":
+            print("\n")
+            continue
+    else:
+        print("Invalid Choice. Please pick 1, 2, 3, or 4.")
