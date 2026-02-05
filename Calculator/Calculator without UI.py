46225e42aa9139c580757c435ebea116426ffdd1
Allen Tolete <allentolete@gmail.com>
2026-02-05 23:48:09 +0800

    Added some shi

---
+++ Calculator/Calculator without UI.py
@@ -0,0 +1,39 @@
+import math
+
+
+
+
+
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    return x / y
+
+print("""
+
+Calculator type shi
+
+Select Operation
+
+[1] Add
+[2] Subtract
+[3] Multiply
+[4] Divide
+
+""")
+
+
+
+
+
+
+
+
